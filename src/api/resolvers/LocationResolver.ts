1379357a41469af46b040e5ef9e58a9034231a09
Joe Moeller <moellejc@gmail.com>
2022-12-20 01:09:56 -0500

    Add some new resolvers for Location and Place

---
+++ src/api/resolvers/LocationResolver.ts
@@ -0,0 +1,36 @@
+import {
+  Arg,
+  Ctx,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
+import { LocationCell } from "../entities/LocationCell";
+import { Place } from "../entities/Place";
+import { PlaceResponse } from "./responses/PlaceResponse";
+import { AppContext } from "../../context/AppContext";
+
+@Resolver()
+export class LocationResolver {
+  @Query(() => Place)
+  async getPlaceFromLocation(
+    @Arg("cell", () => LocationCell) cell: LocationCell,
+    @Ctx() { prisma }: AppContext
+  ): Promise<PlaceResponse> {
+    try {
+      // look for place in data
+
+      // look for place from ReverseGeocode
+
+      // add place to database
+
+      // return place
+
+      return { user: await prisma.user.findUniqueOrThrow({ where: { id } }) };
+    } catch (err) {
+      return { errors: [UserNotFound] };
+    }
+  }
+}
