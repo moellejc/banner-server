2f0b87aa6c6f4b03d18b6ee1b7ddfb812883e958
Joe Moeller <moellejc@gmail.com>
2023-01-08 23:44:52 -0500

    Update class-validator package. Start creating PlaceInput classes

--- src/api/resolvers/PlaceResolver/inputs.ts
+++ src/api/resolvers/PlaceResolver/inputs.ts
@@ -1,7 +1,35 @@
-import { Field, ObjectType } from "type-graphql";
+import { PlaceTypes } from "@prisma/client";
+import { Field, Float, InputType, Int } from "type-graphql";
 
-@ObjectType()
+@InputType()
 export class CreatePlaceInput {
   @Field(() => String)
   name: String;
+
+  @Field(() => PlaceTypes)
+  placeType: PlaceTypes;
+
+  @Field(() => Float)
+  lat: number;
+
+  @Field(() => Float)
+  lon: number;
+}
+
+@InputType()
+export class UpdatePlaceInput {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  name?: String;
+
+  @Field(() => PlaceTypes)
+  placeType?: PlaceTypes;
+
+  @Field(() => Float)
+  lat?: number;
+
+  @Field(() => Float)
+  lon?: number;
 }
